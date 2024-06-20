00ee66d5e6d6b95b2acb22278c60ae1df528e903
Gustavo.N.S <104957030+GustavoNSBr@users.noreply.github.com>
2024-06-20 18:38:24 -0300

    Adicionando arquivo Pilha.h

--- Pilha.h
+++ Pilha.h
@@ -1,1 +1,16 @@
-/*arquivo Pilha.h*/ 
+#ifndef PILHA_H
+#define PILHA_H
+
+typedef struct Pilha {
+    int itens[100];  // Capacidade máxima da pilha
+    int topo;        // Índice do topo da pilha
+} Pilha;
+
+Pilha criar();
+int vazia(Pilha *p);
+void empilhar(Pilha *p, int disco);
+int desempilhar(Pilha *p);
+int topo(Pilha *p);
+int tamanho(Pilha *p);
+
+#endif /* PILHA_H */
