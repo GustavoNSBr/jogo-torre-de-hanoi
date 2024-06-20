9dc953171919928d326c1d3f1aaadd7fe7cd36a6
Arthur Batista de Sousa <142634460+ArthurUCB@users.noreply.github.com>
2024-06-20 18:37:38 -0300

    Adicionando funcionalidades principais do código Principal.c

--- Principal.c
+++ Principal.c
@@ -1,9 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Pilha.h"
 
-int main() {
+// Função para imprimir o estado das torres
+void imprimir(Pilha *torres, int qtd_blocos) {
+    int i, j;
+
+    for (i = qtd_blocos - 1; i >= 0; i--) {
+        for (j = 0; j < 3; j++) {
+            // Pilha j
+            if (tamanho(&torres[j]) > i) {
+                printf("|%d|", torres[j].itens[i]);
+            } else {
+                printf("|*|");
+            }
+
+            printf("\t");
+        }
+        printf("\n");
+    }
+
+    printf("|A|\t|B|\t|C|\n\n");
+}
+
+
+// Função para mover os blocos
+void mover_blocos(Pilha *torres, int qtd_blocos) {
+    int opcao, destino;
 
     while (1) {
+        // Imprimir o estado das torres
+        limpar_tela();
+        imprimir(torres, qtd_blocos);
+
+        // Menu de opções
+        printf("Qual torre escolher?\n");
+        for (int i = 0; i < 3; i++) {
+            printf("%d: Torre %c\n", i + 1, 'A' + i);
+        }
+        printf("4: Voltar ao menu inicial\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 4) {
+            break;
+        }
+
+        if (!validar_opcao(opcao, 3)) {
+            printf("Escolha invalida, tente novamente:\n");
+            limpar_tela();
+            continue;
+        }
+
+        printf("Mover para qual torre?\n");
+        for (int i = 0; i < 3; i++) {
+            if (i + 1 != opcao) {
+                printf("%d: Torre %c\n", i + 1, 'A' + i);
+            }
+        }
+        printf("Opcao: ");
+        scanf("%d", &destino);
+
+        if (!validar_opcao(destino, 3) || destino == opcao) {
+            printf("Escolha invalida, tente novamente:\n");
+            limpar_tela();
+            continue;
+        }
+
+        // Movimentar bloco
+        if (tamanho(&torres[opcao - 1]) == 0) {
+            printf("Movimento invalido! Torre de origem vazia.\n");
+        } else if (tamanho(&torres[destino - 1]) == 0 || topo(&torres[opcao - 1]) < topo(&torres[destino - 1])) {
+            empilhar(&torres[destino - 1], desempilhar(&torres[opcao - 1]));
+        } else {
+            printf("Movimento invalido! Bloco maior na torre de destino.\n");
+        }
+
+        limpar_tela();
+    }
+}
+
+// Função principal
+int main() {
+    int opcao, qtd_blocos;
+
+    while (1) {
+        // Menu de opções inicial
         limpar_tela();
         printf("Escolha uma opcao:\n");
         printf("1: Jogar\n");
@@ -45,6 +127,20 @@ int main() {
             printf("Opcao: ");
             scanf("%d", &qtd_blocos);
         }
+
+        Pilha torres[3];
+
+        for (int i = 0; i < 3; i++) {
+            torres[i] = criar();
+        }
+
+        // Inicialização da primeira torre com os blocos
+        for (int bloco = qtd_blocos; bloco >= 1; bloco--) {
+            empilhar(&torres[0], bloco);
+        }
+
+        // Jogar o jogo
+        mover_blocos(torres, qtd_blocos);
     }
 
     return 0;
